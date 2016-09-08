6f744484a413f841ec612251262d7d4836f1914e
weeryan17 <theweeryan17@gmail.com>
2016-09-08 08:32:18 -0500

    login page work (mostly)

--- js/login.js
+++ js/login.js
@@ -1,6 +1,22 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
-create.onClick(function(){
-	
-});
+document.getElementById("create").onclick = function() {create()};
+var values = {};
+function create() {
+	var x = document.getElementById("register-form");
+	var text = "";
+    var i;
+    for (i = 0; i < x.length-1;i++) {
+        var value = x.elements[i].value;
+        values[i] = value;
+        if(value == ""){
+        	document.getElementById("info").innerHTML = "Invaid Value!";
+        } else {
+        	//values[i] = value;
+        }
+    }
+    window.alert("test");
+    window.alert(values.length());
+    
+}
