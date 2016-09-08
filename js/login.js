f436e08771265186d234d20c386118241a6c6a99
weeryan17 <theweeryan17@gmail.com>
2016-09-08 14:39:50 -0500

    temporary inseption

--- js/login.js
+++ js/login.js
@@ -1,22 +1,19 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
-document.getElementById("create").onclick = function() {create()};
-var values = {};
-function create() {
-	var x = document.getElementById("register-form");
-	var text = "";
-    var i;
-    for (i = 0; i < x.length-1;i++) {
-        var value = x.elements[i].value;
-        values[i] = value;
-        if(value == ""){
-        	document.getElementById("info").innerHTML = "Invaid Value!";
-        } else {
-        	//values[i] = value;
-        }
-    }
-    window.alert("test");
-    window.alert(values.length());
-    
-}
+//document.getElementById("create").onclick = function() {create()};
+//var values = {};
+//function create() {
+//	var x = document.getElementById("register-form");
+//	var text = "";
+//    var i;
+//    for (i = 0; i < x.length-1;i++) {
+//        var value = x.elements[i].value;
+//        if(value == ""){
+//        	document.getElementById("info").innerHTML = "Invaid Value!";
+//        } else {
+//        	//values[i] = value;
+//        }
+//    }
+//    
+//}
