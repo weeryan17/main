7f16bf85d443e2e20017a0a9447e7b003c16ea72
weeryan17 <theweeryan17@gmail.com>
2016-09-26 15:06:34 -0500

    we only have 1 nav bar now :)

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function home(){
+	var frame = document.getElementById('window');
+    frame.src="main.html";
+}
+
+function about(){
+	var frame = document.getElementById('window');
+    frame.src="about.html";
+	
+}
+
+function gallery(){
+	var frame = document.getElementById('window');
+    frame.src="gallery.html";
+	
+}
+
+function contact(){
+	var frame = document.getElementById('window');
+    frame.src="contact.html";
+}
+
+function api(){
+	var frame = document.getElementById('window');
+    frame.src="docs-index.html";
+}
